11863279d66f00e0158c1be0eaae52202bd8dfb5
Aditi Kalangutkar <aditigk9@gmail.com>
2023-08-02 12:07:57 +0530

    ...

--- billeazy-services/src/App.js
+++ billeazy-services/src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route} from "react-router-dom";
 import { AuthContextProvider } from "./context/UserAuthContext";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import Home from "./pages/Home";
-<<<<<<< Updated upstream
 import Agent from "./pages/Agent";
+import Admin from "./pages/Admin";
 
 const App = () => {
   return (
@@ -15,8 +15,8 @@ const App = () => {
     
 
             <Route path='/'  element={<Home/>} />
-<<<<<<< Updated upstream
             <Route path='/employees' element={<Agent/>} />
+            <Route path='/admin' element={<Admin/>} />
             
             {/* <Route path='/rides'  element={<ProtectedRoutes> <Rides/> </ProtectedRoutes>} /> */}
 
