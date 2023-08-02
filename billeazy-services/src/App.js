43ebe5f26d3d596bb93feac895ebceb5cbff5c79
Aditi Kalangutkar <aditigk9@gmail.com>
2023-08-02 11:16:19 +0530

    home routes added

--- billeazy-services/src/App.js
+++ billeazy-services/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     
 
             <Route path='/'  element={<Home/>} />
+            <Route path='/home'  element={<Home/>} />
             <Route path='/employees' element={<Agent/>} />
             
             {/* <Route path='/rides'  element={<ProtectedRoutes> <Rides/> </ProtectedRoutes>} /> */}
