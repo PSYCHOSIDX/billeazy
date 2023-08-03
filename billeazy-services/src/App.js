78efea8dc26f54181f45660e6c3393fecc3669cc
Varad <varadsoham555@gmail.com>
2023-08-03 10:52:39 +0530

    modify upload file

--- billeazy-services/src/App.js
+++ billeazy-services/src/App.js
@@ -5,16 +5,12 @@ import ProtectedAdminRoutes from "./components/ProtectedAdminRoutes";
 
 import Agent from "./pages/Agent";
 import Admin from "./pages/Admin";
-<<<<<<< HEAD
-import Login from "./pages/Login";
 import UploadFile from "./components/UploadFile";
-=======
 
 import AdminLoginPage from "./pages/AdminLoginPage"
 
 import Customer from "./pages/Customer";
 import CustomerLandingPage from "./pages/CustomerLandingPage";
->>>>>>> 9e1c70518c1cad5ac6ac25415506f67e2a0950fc
 
 const App = () => {
   return (
@@ -30,13 +26,9 @@ const App = () => {
             <Route path='/admin' element={<ProtectedAdminRoutes> <Admin/> </ProtectedAdminRoutes>} />
 
             <Route path='/employees' element={<Agent/>} />
-<<<<<<< HEAD
             <Route path='/employees/upload' element={<UploadFile/>} />
-            <Route path='/admin' element={<Admin/>} />
 
-=======
             
->>>>>>> 9e1c70518c1cad5ac6ac25415506f67e2a0950fc
             
             {/* <Route path='/rides'  element={<ProtectedRoutes> <Rides/> </ProtectedRoutes>} /> */}
    
